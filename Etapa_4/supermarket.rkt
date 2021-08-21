9d94814a769664407d4c032fc528a151f90dcba9
maniatro111 <florin.postolache80@gmail.com>
2021-08-21 17:52:45 +0300

    first commit

---
+++ Etapa_4/supermarket.rkt
@@ -0,0 +1,177 @@
+#lang racket
+(require racket/match)
+(require "queue.rkt")
+
+(provide (all-defined-out))
+
+(define ITEMS 5)
+
+(define-struct counter (index closed tt et queue)#:transparent)
+
+; TODO
+; Aveți libertatea să vă structurați programul cum doriți (dar cu restricțiile
+; de mai jos), astfel încât funcția serve să funcționeze conform specificației.
+; 
+; Restricții (impuse de checker):
+; - trebuie să existe în continuare funcția (empty-counter index)
+; - cozile de la case trebuie implementate folosind noul TDA queue
+
+(define (empty-counter index)
+  (make-counter index 0 0 0 empty-queue))
+
+
+(define C1 (empty-counter 1))
+(define C2 (empty-counter 2))
+(define C3 (empty-counter 3))
+(define C4 (empty-counter 4))
+(define C5 (make-counter 5 0 12 8 (queue '((remus . 6) (vivi . 4)) '() 2 0)))
+(define case (list C1 C2 C3 C4 C5))
+
+(define (update f counters index)
+  (update-help f counters index '()))
+
+(define (update-help f counter index acc)
+  (if (null? counter)
+      acc
+      (if (eq? index (counter-index (car counter)))
+          (update-help f (cdr counter) index (append acc (list(f(car counter)))))
+          (update-help f (cdr counter) index (append acc (list(car counter)))))))
+
+(define tt+
+  (λ (minutes)
+    (λ (C)
+      (make-counter (counter-index C) (counter-closed C) (+ (counter-tt C) minutes) (counter-et C) (counter-queue C)))))
+
+(define close-counter
+  (λ (C)
+    (make-counter (counter-index C) 1 (counter-tt C) (counter-et C) (counter-queue C))))
+
+(define et+
+  (λ (minutes)
+    (λ (C)
+      (make-counter (counter-index C) (counter-closed C) (+ (counter-tt C) minutes) (+ (counter-et C) minutes) (counter-queue C)))))
+
+(define (add-to-counter name items)     ; testată de checker
+  (λ (C)                                ; nu modificați felul în care funcția își primește argumentele
+    (if (queue-empty? (counter-queue C))
+        (make-counter (counter-index C) (counter-closed C) (counter-tt ((tt+ items) C)) (counter-et ((et+ items) C)) (enqueue (cons name items) (counter-queue C)))
+        (make-counter (counter-index C) (counter-closed C) (counter-tt ((tt+ items) C)) (counter-et C) (enqueue (cons name items) (counter-queue C)))
+     )))
+
+(define rezolvare
+  (λ (proc)
+    (λ (counters)
+      (if (equal? proc min-tt)
+          (proc counters)
+          (proc counters)))))
+          
+(define min-tt
+  (λ (counters)
+    (foldl (λ (element minim)
+             (if (or (and (< (counter-tt element) (cdr minim)) (equal? (counter-closed element) 0)) (and (eq? (car minim) -1) (equal? (counter-closed element) 0)))
+                 (cons (counter-index element) (counter-tt element))
+                 minim))
+           (cons -1 -1) counters)))
+(define min-et
+  (λ (counters)
+    (foldl (λ (element minim)
+             (if (or (and (< (counter-et element) (cdr minim)) (not(queue-empty? (counter-queue element)))) (and (eq? (car minim) -1) (not(queue-empty? (counter-queue element)))))
+                         (cons (counter-index element) (counter-et element))
+                         minim ))
+                   (cons -1 -1) counters)))
+
+(define (remove-first-from-counter C)   ; testată de checker
+  (make-counter (counter-index C) (counter-closed C) (- (counter-tt C) (counter-et C)) (if (queue-empty? (dequeue(counter-queue C))) 0 (cdr (top (dequeue (counter-queue C))))) (dequeue (counter-queue C))))
+
+(define (pass-time-through-counter minutes)
+  (λ (C)
+    (if (and (equal? (counter-et C) 0) (queue-empty? (counter-queue C)))
+        C
+        (if (and (> minutes (counter-et C)) (queue-empty? (counter-queue C)))
+            (make-counter (counter-index C) (counter-closed C) (- (counter-tt C) (counter-et C)) 0 (counter-queue C))
+            (make-counter (counter-index C) (counter-closed C) (- (counter-tt C) minutes) (- (counter-et C) minutes) (counter-queue C))))))
+
+;=====================================================================================================================================
+  
+; TODO
+; Implementați funcția care simulează fluxul clienților pe la case.
+; ATENȚIE: Față de etapa 3, apare un nou tip de cerere, așadar
+; requests conține 5 tipuri de cereri (cele moștenite din etapa 3 plus una nouă):
+;   - (<name> <n-items>) - persoana <name> trebuie așezată la coadă la o casă              (ca înainte)
+;   - (delay <index> <minutes>) - casa <index> este întârziată cu <minutes> minute         (ca înainte)
+;   - (ensure <average>) - cât timp tt-ul mediu al caselor este mai mare decât
+;                          <average>, se adaugă case fără restricții (case slow)           (ca înainte)
+;   - <x> - trec <x> minute de la ultima cerere, iar starea caselor se actualizează
+;           corespunzător (cu efect asupra câmpurilor tt, et, queue)                       (ca înainte)
+;   - (close <index>) - casa index este închisă                                            (   NOU!   )
+; Sistemul trebuie să proceseze cele 5 tipuri de cereri în ordine, astfel:
+; - persoanele vor fi distribuite la casele DESCHISE cu tt minim; nu se va întâmpla
+;   niciodată ca o persoană să nu poată fi distribuită la nicio casă                       (mică modificare)
+; - când o casă suferă o întârziere, tt-ul și et-ul ei cresc (chiar dacă nu are clienți);
+;   nu aplicați vreun tratament special caselor închise                                    (ca înainte)
+; - tt-ul mediu (ttmed) se calculează pentru toate casele DESCHISE, 
+;   iar la nevoie veți adăuga case slow una câte una, până când ttmed <= <average>         (mică modificare)
+; - când timpul prin sistem avansează cu <x> minute, tt-ul, et-ul și queue-ul tuturor 
+;   caselor se actualizează pentru a reflecta trecerea timpului; dacă unul sau mai mulți 
+;   clienți termină de stat la coadă, ieșirile lor sunt contorizate în ordine cronologică. (ca înainte)
+; - când o casă se închide, ea nu mai primește clienți noi; clienții care erau deja acolo
+;   avansează normal, până la ieșirea din supermarket                                    
+; Rezultatul funcției serve va fi o pereche cu punct între:
+; - lista sortată cronologic a clienților care au părăsit supermarketul:
+;   - fiecare element din listă va avea forma (index_casă . nume)
+;   - dacă mai mulți clienți ies simultan, sortați-i crescător după indexul casei
+; - lista cozilor (de la case DESCHISE sau ÎNCHISE) care încă au clienți:
+;   - fiecare element va avea forma (index_casă . coadă) (coada este de tip queue)
+;   - lista este sortată după indexul casei
+(define (serve requests fast-counters slow-counters)
+  (serve-helper requests fast-counters slow-counters '()))
+
+(define (serve-helper requests fast-counters slow-counters acc)
+  (if (null? requests)
+      (afisare acc (append fast-counters slow-counters))
+      (match (car requests)
+        [(list 'close index-casa) (serve-helper (cdr requests) (update close-counter fast-counters index-casa) (update close-counter slow-counters index-casa) acc) ]
+        [(list 'ensure average)(if (<= (/ (foldl (λ (element acc) (if (equal? (counter-closed element) 0)
+                                                                      (+ acc (counter-tt element))
+                                                                      acc)) 0 (append fast-counters slow-counters)) (foldl (λ (element acc)(if (equal? (counter-closed element) 0)
+                                                                                                                                               (add1 acc)
+                                                                                                                                               acc))0 (append fast-counters slow-counters))) average)
+                                   (serve-helper (cdr requests) fast-counters slow-counters acc)
+                                   (serve-helper requests fast-counters (append slow-counters (list (empty-counter (+ (length (append fast-counters slow-counters)) 1))))acc))]
+        [(list 'delay index minutes) (serve-helper (cdr requests) (update (et+ minutes) fast-counters index) (update (et+ minutes) slow-counters index) acc)]
+        [(list name items) (if (<= items ITEMS)
+                              (serve-helper (cdr requests) (update (add-to-counter name items) fast-counters (car (min-tt(append fast-counters slow-counters))))
+                                    (update (add-to-counter name items) slow-counters (car (min-tt(append fast-counters slow-counters)))) acc)
+                              (serve-helper (cdr requests) fast-counters (update (add-to-counter name items) slow-counters (car (min-tt slow-counters))) acc))]
+        [number (serve-helper (cdr requests)
+                              (foldl (λ (element acc)(append acc (list ((modify-counter number) element))))'() fast-counters)
+                              (foldl (λ (element acc)(append acc (list ((modify-counter number) element))))'() slow-counters)
+                              (append acc (((passing-time number) (append fast-counters slow-counters)) '())))])))
+
+
+(define (modify-counter minutes)
+  (λ (C)
+    (if (and (>= minutes (counter-et C)) (not(queue-empty? (counter-queue C)))) ((modify-counter (- minutes (counter-et C))) (remove-first-from-counter C))
+        ((pass-time-through-counter minutes) C) )))
+(define (get-ones-out minutes)
+  (λ (C)
+    (λ (acc)
+      (if (and (>= minutes (counter-et C)) (not (queue-empty? (counter-queue C))))
+          (((get-ones-out (- minutes (counter-et C))) (remove-first-from-counter C)) (append acc (list(cons (counter-index C) (car(top (counter-queue C)))))))
+           acc))))
+(define (passing-time minutes)
+  (λ (counters)
+    (λ (acc)
+      (if (equal? minutes 0) acc
+          (((passing-time (- minutes 1))
+            (foldl (λ (element acc)(append acc (list ((modify-counter 1) element))))'() counters))
+           (append acc (foldl (λ (element acc)
+                                (if (empty? (((get-ones-out 1) element) '()))
+                                    acc
+                                    (append acc (((get-ones-out 1) element) '())) ))'() counters))) ))))
+
+(define (afisare persoane_iesire counters)
+  (cons persoane_iesire (foldl (λ (C acc)
+                               (if (queue-empty? (counter-queue C))
+                                   acc
+                                   (append acc (list(cons (counter-index C) (counter-queue C)))))) '() counters)))
---
+++ README.md
@@ -0,0 +1,1 @@
+# supermarket-simulation-in-racket
